5477b6eb2d587264c2a0909fb958f4462fe6933b
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-08-03 20:39:42 +0200

    work

--- chuboardpainter_main.py
+++ chuboardpainter_main.py
@@ -2,3 +2,4 @@ from classes.chu_board_painter import ChuBoardPainter
 
 mychuboardpainter = ChuBoardPainter()
 mychuboardpainter.load_file(".\\positions\\chu_shogi.json")
+mychuboardpainter.create_board_image_and_save(".\\boardimages\\chu_shogi.png")
