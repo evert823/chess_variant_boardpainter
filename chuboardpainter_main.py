771c5c1b0ffcca20fb52ca765c7741b91a5fb20a
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-08-06 10:49:30 +0200

    Demo promotions

--- chuboardpainter_main.py
+++ chuboardpainter_main.py
@@ -1,5 +1,7 @@
 from classes.chu_board_painter import ChuBoardPainter
 
 mychuboardpainter = ChuBoardPainter()
-mychuboardpainter.load_file(".\\positions\\chu_shogi.json")
-mychuboardpainter.create_board_image_and_save(".\\boardimages\\chu_shogi.png")
+mychuboardpainter.load_file(".\\positions\\chu_shogi_initial_position.json")
+mychuboardpainter.create_board_image_and_save(".\\boardimages\\chu_shogi_initial_position.png")
+mychuboardpainter.load_file(".\\positions\\chu_shogi_show_promotions.json")
+mychuboardpainter.create_board_image_and_save(".\\boardimages\\chu_shogi_show_promotions.png")
