a4211e6e004523f2873cbb35c8a561dba9e60726
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-08-13 21:11:15 +0200

    Here storing variant invented by GHCP

--- other_shogi_variants_main.py
+++ other_shogi_variants_main.py
@@ -12,3 +12,7 @@ mychuboardpainter.create_board_image_and_save(".\\shogi_variants\\boardimages\\m
 
 mychuboardpainter.load_file(".\\shogi_variants\\positions\\tai_initial_position.json")
 mychuboardpainter.create_board_image_and_save(".\\shogi_variants\\boardimages\\tai_initial_position_set2.png")
+
+mychuboardpainter.pieceimages_folder = "..\\chu_shogi_piece_images\\output_set2"
+mychuboardpainter.load_file(".\\shogi_variants\\positions\\invented_by_GHPC.json")
+mychuboardpainter.create_board_image_and_save(".\\shogi_variants\\boardimages\\invented_by_GHPC.png")
