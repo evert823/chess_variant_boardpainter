01afe23b9c568b430ae3fd3ce304c73a73885dca
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-08-31 17:06:06 +0200

    More flexible mainscript

--- boardpainter_main.py
+++ boardpainter_main.py
@@ -1,20 +1,24 @@
 from classes.board_painter import BoardPainter
 
 myboardpainter = BoardPainter()
-myboardpainter.load_file(".\\positions\\personA_personB.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\personA_personB.png")
-myboardpainter.load_file(".\\positions\\personC_personD.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\personC_personD.png")
-myboardpainter.load_file(".\\positions\\kr_nn_2char.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\kr_nn_2char.png")
+
+positionpath = ".\\positions"
+boardpath = ".\\boardimages"
+
+myboardpainter.load_file(f"{positionpath}\\personA_personB.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\personA_personB.png")
+myboardpainter.load_file(f"{positionpath}\\personC_personD.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\personC_personD.png")
+myboardpainter.load_file(f"{positionpath}\\kr_nn_2char.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\kr_nn_2char.png")
 
 myboardpainter.pieceimages_folder = "pieceimages_classicwood"
 myboardpainter.pieceimages_extension = "png"
 myboardpainter.a1_is_white = False
 
-myboardpainter.load_file(".\\positions\\test_classicwood.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\test_classicwood.png")
-myboardpainter.load_file(".\\positions\\dark_forest_initial.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\dark_forest_initial.png")
-myboardpainter.load_file(".\\positions\\dumbarton_initial.json")
-myboardpainter.create_board_image_and_save(".\\boardimages\\dumbarton_initial.png")
+myboardpainter.load_file(f"{positionpath}\\test_classicwood.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\test_classicwood.png")
+myboardpainter.load_file(f"{positionpath}\\dark_forest_initial.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\dark_forest_initial.png")
+myboardpainter.load_file(f"{positionpath}\\dumbarton_initial.json")
+myboardpainter.create_board_image_and_save(f"{boardpath}\\dumbarton_initial.png")
